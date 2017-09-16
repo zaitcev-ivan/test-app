48ed173d3853ee22c995098f8c7edddb5f7734a6
Zaytcev Ivan <ii.zaytcev@gmail.com>
2017-09-16 10:01:38 +0400

    Создание таблицы Users, правка пути к файлу базы sqlite в конфигурационнном файле

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'sqlite:'.__DIR__ . '/sqlite.db',
+    'dsn' => 'sqlite:'.__DIR__ . '/data/sqlite.db',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
