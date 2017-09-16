1f79425e7b1a90ef8066193731fc6258ae4d5323
Zaytcev Ivan <ii.zaytcev@gmail.com>
2017-09-16 09:43:37 +0400

    Конфигурация БД SQLite и создание директории для хренения базы данных

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'sqlite:'.__DIR__ . '/sqlite.db',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
